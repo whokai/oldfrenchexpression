186f523c109e6da78ec59221fa70169835a30326
EC2 Default User <ec2-user@ip-172-31-21-184.us-east-2.compute.internal>
2019-07-12 13:22:39 +0000

    first commit

--- README.md
+++ README.md
@@ -24,3 +24,5 @@ Things you may want to cover:
 * ...
 # oldfrenchexpression
 # oldfrenchexpression
+# oldfrenchexpression
+# oldfrenchexpression
---
+++ app/bot/bot.rb
@@ -0,0 +1,9 @@
+require "facebokk-messenger"
+include Facebook::Messenger
+Facebook::Messenger::Subscriptions.subscribe(
+  access_token: access_token,
+  subscribed_fields: %w[feed mention name]
+)
+Bot.on :message do |message|
+  message.reply(text: 'Hello, human!')
+end
