be49900818f5e67162468b21ff63bada97312869
Whokai <jerome.riglet@outlook.com>
2019-07-15 14:24:57 +0000

    first commit

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,9 +1,7 @@
-require "facebokk-messenger"
+require "facebook/messenger"
 include Facebook::Messenger
-Facebook::Messenger::Subscriptions.subscribe(
-  access_token: access_token,
-  subscribed_fields: %w[feed mention name]
-)
+Facebook::Messenger::Subscriptions.subscribe(access_token: access_token)
+
 Bot.on :message do |message|
   message.reply(text: 'Hello, human!')
 end
--- config/application.rb
+++ config/application.rb
@@ -8,10 +8,13 @@ Bundler.require(*Rails.groups)
 
 module BotMessenger
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+    config.paths.add File.join('app', 'bot'), glob: File.join('**', '*.rb')
+    config.autoload_paths += Dir[Rails.root.join('app', 'bot', '*')]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.paths.add File.join('app', 'bot'), glob: File.join('**', '*.rb')
-    config.autoload_paths += Dir[Rails.root.join('app', 'bot', '*')]
+    
   end
 end
