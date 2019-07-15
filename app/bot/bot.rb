ea2bb68796b89b6f30cc8aaf48def32393cbe59c
Whokai <jerome.riglet@outlook.com>
2019-07-15 14:56:32 +0000

    token acces changed

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,6 +1,9 @@
 require "facebook/messenger"
 include Facebook::Messenger
-Facebook::Messenger::Subscriptions.subscribe(access_token: access_token)
+Facebook::Messenger::Subscriptions.subscribe(
+  access_token: ENV["ACCESS-TOKEN"],
+  subscribed_fields: %w[feed mention name]
+)
 
 Bot.on :message do |message|
   message.reply(text: 'Hello, human!')
